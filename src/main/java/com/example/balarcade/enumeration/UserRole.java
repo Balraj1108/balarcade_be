03882ef4439578fc231d0fdfbfbc035771fbf621
Balraj Singh <b.singh@solvingteam.it>
2024-11-27 16:11:05 +0100

    setup db mysql and create User

---
+++ src/main/java/com/example/balarcade/enumeration/UserRole.java
@@ -0,0 +1,14 @@
+package com.example.balarcade.enumeration;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum UserRole {
+
+    ADMIN,
+    USER
+
+}
+
